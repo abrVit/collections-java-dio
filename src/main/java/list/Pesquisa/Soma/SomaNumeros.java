e38039ec9b1649a4e733de2ab9a02dec95a671a0
abrVit <github.outclass778@passmail.net>
2026-05-26 22:33:37 -0300

    Incluindo classe SomaNumeros() ao projeto; Classe para estudo de pesquisa em Listas
    Signed-off-by: abrVit <github.outclass778@passmail.net>

---
+++ src/main/java/list/Pesquisa/Soma/SomaNumeros.java
@@ -0,0 +1,79 @@
+package main.java.list.Pesquisa.Soma;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SomaNumeros {
+
+    List<Integer> listaNumeros;
+
+    public SomaNumeros() {
+        this.listaNumeros = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int num){listaNumeros.add(num);}
+
+    public int calcularSoma(){
+        int result = 0;
+        for (int num : listaNumeros) {
+            result += num;
+        }
+        return result;
+    }
+
+    public int encontrarMaiorNumero(){
+        int maiorNum = listaNumeros.get(0);
+        int j = listaNumeros.size();
+        if (!listaNumeros.isEmpty()) {
+            for (int i = 0; i < j; i++) {
+                if (maiorNum < listaNumeros.get(i)) {
+                    maiorNum = listaNumeros.get(i);
+                }
+            }
+            return maiorNum;
+        } else {
+            throw new RuntimeException("Lista esta vazia");
+        }
+    }
+
+    public int enocntrarMenorNumero(){
+        int menorNum = listaNumeros.get(0);
+        int n = listaNumeros.size();
+        if (!listaNumeros.isEmpty()) {
+            for (int i = 0; i < n; i++) {
+                if (menorNum > listaNumeros.get(i)) {
+                    menorNum = listaNumeros.get(i);
+                }
+            }
+            return menorNum;
+        } else {
+            throw new RuntimeException("Lista esta vazia");
+        }
+    }
+
+    public void exibirNumeros() {
+        if (listaNumeros.isEmpty()) {
+            System.out.println("Lista Vazia");
+        } else {
+            System.out.println(this.listaNumeros);
+        }
+    }
+
+
+    public static void main(String[] args){
+        SomaNumeros lista = new SomaNumeros();
+        lista.adicionarNumero(10);
+        lista.adicionarNumero(20);
+        lista.adicionarNumero(-45);
+        lista.adicionarNumero(5);
+        lista.adicionarNumero(60);
+        lista.adicionarNumero(80);
+        lista.adicionarNumero(70);
+        lista.adicionarNumero(15);
+
+        lista.exibirNumeros();
+        System.out.println(lista.calcularSoma());
+        System.out.println(lista.enocntrarMenorNumero());
+        System.out.println(lista.encontrarMaiorNumero());
+    }
+}
