72a2bb28802ffa9e3b0e651b9773b89c15334f6f
abrVit <github.outclass778@passmail.net>
2026-05-29 07:19:23 -0300

    atualizacao classes package Pesquisa
    Signed-off-by: abrVit <github.outclass778@passmail.net>

--- src/main/java/list/Pesquisa/Livros/CatalogoLivros.java
+++ src/main/java/list/Pesquisa/Livros/CatalogoLivros.java
@@ -1,7 +1,6 @@
 package main.java.list.Pesquisa.Livros;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CatalogoLivros {
 
--- src/main/java/list/Pesquisa/Soma/SomaNumeros.java
+++ src/main/java/list/Pesquisa/Soma/SomaNumeros.java
@@ -1,7 +1,6 @@
 package main.java.list.Pesquisa.Soma;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class SomaNumeros {
 
